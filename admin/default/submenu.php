6be92b173ff75288fe0199e73a7244afa11499cf
Syauqi <sauki084@gmail.com>
2026-01-05 04:56:08 +0700

    Modify file submenu and add file management tools

--- admin/default/submenu.php
+++ admin/default/submenu.php
@@ -22,6 +22,8 @@ $menu[] = array('Header', __('SHORTCUT'));
 $menu['user-profile'] = array(__('Change User Profiles'), MWB.'system/app_user.php?changecurrent=true&action=detail', __('Change Current User Profiles and Password'));
 $menu[] = array(__('Shortcut Setting'), MWB.'system/shortcut.php', __('Shortcut Setting'));
 $menu[] = array(__('Theme'), MWB.'system/theme.php', __('Configure theme Preferences'));
+$menu[] = array(__('Management Tools'), MWB . 'system/management_tools.php', __('Various Management Tools'));
+
 
 /*
 if (utility::havePrivilege('bibliography', 'r') AND utility::havePrivilege('bibliography', 'w')) {
